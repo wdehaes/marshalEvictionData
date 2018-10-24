a738361447d447b1684dba3f0120dcbcea4512cd
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2018-10-24 18:12:46 -0400

    Switched to just NYC zip codes

--- Evictions.R
+++ Evictions.R
@@ -17,8 +17,8 @@ duplicates <- residentialEvictiontable_2017_withDups[duplicated(residentialEvict
 evicZips <- data.frame(table(residentialEvictiontable_2017$EVICTION_ZIP)) #creates a new table with all the zip codes with evictions and the number of evictions in each
 colnames(evicZips) <- c('Zip.Code', 'evictions') #rename columns
 
-masterZips <- read.csv("C:/Users/aaron/OneDrive/Documents/Columbia Journalism/Universal Access to Counsel/marshalEvictionData/NYSzips.csv") #master list of every zip code in NYS
-masterZips <- merge(masterZips, evicZips, by = "Zip.Code", all.x = TRUE) #merges basted based on common zip codes
+masterZips <- read.csv("C:/Users/aaron/OneDrive/Documents/Columbia Journalism/Universal Access to Counsel/marshalEvictionData/NYczips.csv") #master list of every zip code in NYS
+masterZips <- merge(masterZips, evicZips, by = "Zip.Code", all.x = TRUE) #merges based on common zip codes
 masterZips[is.na(masterZips)] <- 0 # sets all na values equal to zero
 
 #Population data by zip code
@@ -30,6 +30,8 @@ masterZips <- merge(masterZips, zipPops, by = "Zip.Code", all.x = TRUE) #merges 
 zipIncome <- read.csv("C:/Users/aaron/OneDrive/Documents/Columbia Journalism/Universal Access to Counsel/marshalEvictionData/medianHouseholdIncomebyZip.csv") #Imports income data
 zipIncome$Zip.Code <- as.numeric(str_extract(zipIncome$Zip.Code, "[0-9]{5}")) #cleans the string so we're just working with the zip code numbers
 masterZips <- merge(masterZips, zipIncome, by = "Zip.Code", all.x = TRUE) #merges tables based on common zip codes
+
+masterZips <- masterZips[!duplicated(masterZips),]
 
 export <- toJSON(masterZips) #converts dataframe to json format
 
