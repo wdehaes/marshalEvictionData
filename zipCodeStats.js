d5a8e4bb321747fcfaa117225fe498b32ead997d
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2018-11-01 21:28:27 -0400

    Updating github with offline work

---
+++ zipCodeStats.js
@@ -0,0 +1,739 @@
+var zipCodeStats = {
+    "10001": {
+        "evictions": 46,
+        "population": 21102
+    },
+    "10002": {
+        "evictions": 77,
+        "population": 81410
+    },
+    "10003": {
+        "evictions": 42,
+        "population": 56024
+    },
+    "10004": {
+        "evictions": 3,
+        "population": 3089
+    },
+    "10005": {
+        "evictions": 14,
+        "population": 7135
+    },
+    "10006": {
+        "evictions": 7,
+        "population": 3011
+    },
+    "10007": {
+        "evictions": 5,
+        "population": 6988
+    },
+    "10009": {
+        "evictions": 102,
+        "population": 61347
+    },
+    "10010": {
+        "evictions": 23,
+        "population": 31834
+    },
+    "10011": {
+        "evictions": 43,
+        "population": 50984
+    },
+    "10012": {
+        "evictions": 25,
+        "population": 24090
+    },
+    "10013": {
+        "evictions": 24,
+        "population": 27700
+    },
+    "10014": {
+        "evictions": 29,
+        "population": 31959
+    },
+    "10016": {
+        "evictions": 54,
+        "population": 54183
+    },
+    "10017": {
+        "evictions": 23,
+        "population": 16575
+    },
+    "10018": {
+        "evictions": 11,
+        "population": 5229
+    },
+    "10019": {
+        "evictions": 95,
+        "population": 42870
+    },
+    "10021": {
+        "evictions": 40,
+        "population": 43631
+    },
+    "10022": {
+        "evictions": 33,
+        "population": 31924
+    },
+    "10023": {
+        "evictions": 37,
+        "population": 60998
+    },
+    "10024": {
+        "evictions": 32,
+        "population": 59283
+    },
+    "10025": {
+        "evictions": 115,
+        "population": 94600
+    },
+    "10026": {
+        "evictions": 104,
+        "population": 34003
+    },
+    "10027": {
+        "evictions": 129,
+        "population": 59707
+    },
+    "10028": {
+        "evictions": 36,
+        "population": 45141
+    },
+    "10029": {
+        "evictions": 175,
+        "population": 76003
+    },
+    "10030": {
+        "evictions": 129,
+        "population": 26999
+    },
+    "10031": {
+        "evictions": 189,
+        "population": 56438
+    },
+    "10032": {
+        "evictions": 167,
+        "population": 57331
+    },
+    "10033": {
+        "evictions": 154,
+        "population": 53926
+    },
+    "10034": {
+        "evictions": 112,
+        "population": 38908
+    },
+    "10035": {
+        "evictions": 139,
+        "population": 33969
+    },
+    "10036": {
+        "evictions": 93,
+        "population": 24711
+    },
+    "10037": {
+        "evictions": 121,
+        "population": 17416
+    },
+    "10038": {
+        "evictions": 20,
+        "population": 20300
+    },
+    "10039": {
+        "evictions": 120,
+        "population": 24527
+    },
+    "10040": {
+        "evictions": 113,
+        "population": 41905
+    },
+    "10044": {
+        "evictions": 15,
+        "population": 11661
+    },
+    "10065": {
+        "evictions": 35,
+        "population": 32270
+    },
+    "10069": {
+        "evictions": 7,
+        "population": 5199
+    },
+    "10075": {
+        "evictions": 9,
+        "population": 26121
+    },
+    "10101": {
+        "evictions": 1
+    },
+    "10128": {
+        "evictions": 57,
+        "population": 60453
+    },
+    "10280": {
+        "evictions": 9,
+        "population": 7853
+    },
+    "10282": {
+        "evictions": 3,
+        "population": 4783
+    },
+    "10301": {
+        "evictions": 121,
+        "population": 39706
+    },
+    "10302": {
+        "evictions": 36,
+        "population": 19088
+    },
+    "10303": {
+        "evictions": 86,
+        "population": 26337
+    },
+    "10304": {
+        "evictions": 131,
+        "population": 42193
+    },
+    "10305": {
+        "evictions": 58,
+        "population": 41749
+    },
+    "10306": {
+        "evictions": 57,
+        "population": 55909
+    },
+    "10307": {
+        "evictions": 9,
+        "population": 14096
+    },
+    "10308": {
+        "evictions": 13,
+        "population": 27357
+    },
+    "10309": {
+        "evictions": 21,
+        "population": 32519
+    },
+    "10310": {
+        "evictions": 63,
+        "population": 24962
+    },
+    "10312": {
+        "evictions": 30,
+        "population": 59304
+    },
+    "10314": {
+        "evictions": 67,
+        "population": 85510
+    },
+    "10451": {
+        "evictions": 264,
+        "population": 45713
+    },
+    "10452": {
+        "evictions": 433,
+        "population": 75371
+    },
+    "10453": {
+        "evictions": 579,
+        "population": 78309
+    },
+    "10454": {
+        "evictions": 142,
+        "population": 37337
+    },
+    "10455": {
+        "evictions": 220,
+        "population": 39665
+    },
+    "10456": {
+        "evictions": 536,
+        "population": 86547
+    },
+    "10457": {
+        "evictions": 526,
+        "population": 70496
+    },
+    "10458": {
+        "evictions": 669,
+        "population": 79492
+    },
+    "10459": {
+        "evictions": 335,
+        "population": 47308
+    },
+    "10460": {
+        "evictions": 470,
+        "population": 57311
+    },
+    "10461": {
+        "evictions": 128,
+        "population": 50502
+    },
+    "10462": {
+        "evictions": 336,
+        "population": 75784
+    },
+    "10463": {
+        "evictions": 205,
+        "population": 67970
+    },
+    "10464": {
+        "evictions": 12,
+        "population": 4534
+    },
+    "10465": {
+        "evictions": 51,
+        "population": 42230
+    },
+    "10466": {
+        "evictions": 362,
+        "population": 67813
+    },
+    "10467": {
+        "evictions": 615,
+        "population": 97060
+    },
+    "10468": {
+        "evictions": 532,
+        "population": 76103
+    },
+    "10469": {
+        "evictions": 207,
+        "population": 66631
+    },
+    "10470": {
+        "evictions": 86,
+        "population": 15293
+    },
+    "10471": {
+        "evictions": 46,
+        "population": 22922
+    },
+    "10472": {
+        "evictions": 329,
+        "population": 66358
+    },
+    "10473": {
+        "evictions": 173,
+        "population": 58519
+    },
+    "10474": {
+        "evictions": 79,
+        "population": 12281
+    },
+    "10475": {
+        "evictions": 81,
+        "population": 40931
+    },
+    "11001": {
+        "evictions": 1,
+        "population": 26883
+    },
+    "11004": {
+        "evictions": 6,
+        "population": 14016
+    },
+    "11040": {
+        "evictions": 1,
+        "population": 40782
+    },
+    "11101": {
+        "evictions": 53,
+        "population": 25484
+    },
+    "11102": {
+        "evictions": 44,
+        "population": 34133
+    },
+    "11103": {
+        "evictions": 52,
+        "population": 38780
+    },
+    "11104": {
+        "evictions": 42,
+        "population": 27232
+    },
+    "11105": {
+        "evictions": 44,
+        "population": 36688
+    },
+    "11106": {
+        "evictions": 53,
+        "population": 38875
+    },
+    "11109": {
+        "evictions": 10,
+        "population": 3523
+    },
+    "11201": {
+        "evictions": 56,
+        "population": 51128
+    },
+    "11203": {
+        "evictions": 317,
+        "population": 76174
+    },
+    "11204": {
+        "evictions": 69,
+        "population": 78134
+    },
+    "11205": {
+        "evictions": 79,
+        "population": 40366
+    },
+    "11206": {
+        "evictions": 135,
+        "population": 81677
+    },
+    "11207": {
+        "evictions": 406,
+        "population": 93386
+    },
+    "11208": {
+        "evictions": 417,
+        "population": 94469
+    },
+    "11209": {
+        "evictions": 112,
+        "population": 68853
+    },
+    "11210": {
+        "evictions": 217,
+        "population": 62008
+    },
+    "11211": {
+        "evictions": 56,
+        "population": 90117
+    },
+    "11212": {
+        "evictions": 468,
+        "population": 84500
+    },
+    "11213": {
+        "evictions": 290,
+        "population": 63767
+    },
+    "11214": {
+        "evictions": 111,
+        "population": 88630
+    },
+    "11215": {
+        "evictions": 16,
+        "population": 63488
+    },
+    "11216": {
+        "evictions": 162,
+        "population": 54316
+    },
+    "11217": {
+        "evictions": 36,
+        "population": 35881
+    },
+    "11218": {
+        "evictions": 82,
+        "population": 75220
+    },
+    "11219": {
+        "evictions": 59,
+        "population": 92221
+    },
+    "11220": {
+        "evictions": 88,
+        "population": 99598
+    },
+    "11221": {
+        "evictions": 284,
+        "population": 78895
+    },
+    "11222": {
+        "evictions": 37,
+        "population": 36934
+    },
+    "11223": {
+        "evictions": 87,
+        "population": 78731
+    },
+    "11224": {
+        "evictions": 107,
+        "population": 47621
+    },
+    "11225": {
+        "evictions": 223,
+        "population": 56829
+    },
+    "11226": {
+        "evictions": 487,
+        "population": 101572
+    },
+    "11228": {
+        "evictions": 33,
+        "population": 41788
+    },
+    "11229": {
+        "evictions": 90,
+        "population": 80018
+    },
+    "11230": {
+        "evictions": 122,
+        "population": 86408
+    },
+    "11231": {
+        "evictions": 29,
+        "population": 33336
+    },
+    "11232": {
+        "evictions": 31,
+        "population": 28265
+    },
+    "11233": {
+        "evictions": 368,
+        "population": 67053
+    },
+    "11234": {
+        "evictions": 139,
+        "population": 87757
+    },
+    "11235": {
+        "evictions": 132,
+        "population": 79132
+    },
+    "11236": {
+        "evictions": 251,
+        "population": 93877
+    },
+    "11237": {
+        "evictions": 77,
+        "population": 49896
+    },
+    "11238": {
+        "evictions": 102,
+        "population": 49262
+    },
+    "11239": {
+        "evictions": 88,
+        "population": 13393
+    },
+    "11240": {
+        "evictions": 1
+    },
+    "11242": {
+        "evictions": 1
+    },
+    "11249": {
+        "evictions": 28
+    },
+    "11306": {
+        "evictions": 1
+    },
+    "11354": {
+        "evictions": 63,
+        "population": 54878
+    },
+    "11355": {
+        "evictions": 91,
+        "population": 85871
+    },
+    "11356": {
+        "evictions": 57,
+        "population": 23438
+    },
+    "11357": {
+        "evictions": 19,
+        "population": 39150
+    },
+    "11358": {
+        "evictions": 22,
+        "population": 37546
+    },
+    "11359": {
+        "evictions": 2,
+        "population": 0
+    },
+    "11360": {
+        "evictions": 14,
+        "population": 18884
+    },
+    "11361": {
+        "evictions": 23,
+        "population": 28606
+    },
+    "11362": {
+        "evictions": 6,
+        "population": 17823
+    },
+    "11363": {
+        "evictions": 3,
+        "population": 6988
+    },
+    "11364": {
+        "evictions": 16,
+        "population": 34555
+    },
+    "11365": {
+        "evictions": 65,
+        "population": 42252
+    },
+    "11366": {
+        "evictions": 8,
+        "population": 13532
+    },
+    "11367": {
+        "evictions": 74,
+        "population": 41047
+    },
+    "11368": {
+        "evictions": 231,
+        "population": 109931
+    },
+    "11369": {
+        "evictions": 38,
+        "population": 38615
+    },
+    "11370": {
+        "evictions": 27,
+        "population": 39688
+    },
+    "11372": {
+        "evictions": 124,
+        "population": 66636
+    },
+    "11373": {
+        "evictions": 123,
+        "population": 100820
+    },
+    "11374": {
+        "evictions": 68,
+        "population": 43600
+    },
+    "11375": {
+        "evictions": 67,
+        "population": 68733
+    },
+    "11377": {
+        "evictions": 88,
+        "population": 89830
+    },
+    "11378": {
+        "evictions": 47,
+        "population": 34981
+    },
+    "11379": {
+        "evictions": 28,
+        "population": 34821
+    },
+    "11384": {
+        "evictions": 3
+    },
+    "11385": {
+        "evictions": 193,
+        "population": 98592
+    },
+    "11411": {
+        "evictions": 21,
+        "population": 18556
+    },
+    "11412": {
+        "evictions": 82,
+        "population": 34882
+    },
+    "11413": {
+        "evictions": 101,
+        "population": 38912
+    },
+    "11414": {
+        "evictions": 28,
+        "population": 26148
+    },
+    "11415": {
+        "evictions": 45,
+        "population": 19341
+    },
+    "11416": {
+        "evictions": 57,
+        "population": 24861
+    },
+    "11417": {
+        "evictions": 39,
+        "population": 28967
+    },
+    "11418": {
+        "evictions": 79,
+        "population": 36256
+    },
+    "11419": {
+        "evictions": 74,
+        "population": 47211
+    },
+    "11420": {
+        "evictions": 87,
+        "population": 44354
+    },
+    "11421": {
+        "evictions": 56,
+        "population": 39127
+    },
+    "11422": {
+        "evictions": 74,
+        "population": 30425
+    },
+    "11423": {
+        "evictions": 79,
+        "population": 29987
+    },
+    "11426": {
+        "evictions": 12,
+        "population": 17590
+    },
+    "11427": {
+        "evictions": 40,
+        "population": 23593
+    },
+    "11428": {
+        "evictions": 42,
+        "population": 19168
+    },
+    "11429": {
+        "evictions": 55,
+        "population": 25105
+    },
+    "11432": {
+        "evictions": 171,
+        "population": 60809
+    },
+    "11433": {
+        "evictions": 157,
+        "population": 32687
+    },
+    "11434": {
+        "evictions": 159,
+        "population": 59129
+    },
+    "11435": {
+        "evictions": 155,
+        "population": 53687
+    },
+    "11436": {
+        "evictions": 45,
+        "population": 17949
+    },
+    "11439": {
+        "evictions": 2
+    },
+    "11691": {
+        "evictions": 289,
+        "population": 60035
+    },
+    "11692": {
+        "evictions": 120,
+        "population": 18540
+    },
+    "11693": {
+        "evictions": 33,
+        "population": 11916
+    },
+    "11694": {
+        "evictions": 53,
+        "population": 20408
+    }
+}
